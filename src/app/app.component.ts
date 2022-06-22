dd7b689c9cee3e8edc9b3091b87dce2da817fd35
Max ubuntu dev <ghalem.maxime@gmail.com>
2022-06-22 15:06:32 +0200

    directive *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component , OnInit } from '@angular/core';
 import { Pokemon } from './pokemon';
 import { POKEMONS } from './mock-pokemon';
-import { catchError } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -14,16 +13,10 @@ export class AppComponent implements OnInit {
   title = 'ng-pokemon-app';
   pokemonListe: Pokemon []= POKEMONS;
 
-  idSelected: number  = 0;
-
   pokemonSelected: Pokemon|undefined ;
 
   ngOnInit(): void {
     console.table(this.pokemonListe)
-
-    if (this.idSelected){
-      this.idSelected = 0;
-    }
 
   }
 
