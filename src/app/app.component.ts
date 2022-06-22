265960a41d4cfff8987d422ed408391ca78824c4
Max ubuntu dev <ghalem.maxime@gmail.com>
2022-06-22 11:25:26 +0200

    1er phase de cours avec création d'objet simple est déclaration de tableau via mock

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component , OnInit } from '@angular/core';
+import { Pokemon } from './pokemon';
+import { POKEMONS } from './mock-pokemon';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Welcome to {{title}}!</h1>`
+  template: `<h1>Welcome world {{pokemonListe[2].name}} </h1>
+  <p>Pokemon de type {{pokemonListe[2].types }}</p>
+  <p>Date de création de l'objet {{pokemonListe[2].created }}</p>`
+
 
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'ng-pokemon-app';
+  pokemonListe: Pokemon [];
+
+  ngOnInit(): void {
+    console.log(`initialisation du composant racine : AppComponent `);
+    this.pokemonListe = POKEMONS;
+    console.table(this.pokemonListe)
+    this.selectionPokemon(this.pokemonListe[0])
+
+  }
+
+  selectionPokemon(pokemon: Pokemon ) {
+    console.log(`Vous avez choisi ${pokemon.name}`)
+  }
+
 }
