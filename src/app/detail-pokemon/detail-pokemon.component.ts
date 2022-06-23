bcc0f86b0d205350942fafa7a2dd5257012c18b1
Max ubuntu dev <ghalem.maxime@gmail.com>
2022-06-23 15:01:13 +0200

    creation de template detail-pokemon

--- src/app/detail-pokemon/detail-pokemon.component.ts
+++ src/app/detail-pokemon/detail-pokemon.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { POKEMONS } from '../mock-pokemon';
+import { Pokemon } from '../pokemon';
 
 @Component({
   selector: 'app-detail-pokemon',
@@ -8,9 +11,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailPokemonComponent implements OnInit {
 
-  constructor() { }
+  pokemonListe: Pokemon []= POKEMONS;
+  pokemon: Pokemon |undefined;
+
+  constructor(private ActivRouter: ActivatedRoute) { }
 
   ngOnInit(): void {
+    
+    // get Id via path url 
+    const pokemonId: string|null = this.ActivRouter.snapshot.paramMap.get('id');
+
+    if (pokemonId){
+      this.pokemon = this.pokemonListe.find(pk => pk.id == +pokemonId)
+    }
+    
+
   }
 
 }
