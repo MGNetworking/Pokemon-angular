59a2a0dcf2d7007428a4b412d65dbaa30a38ea55
Max ubuntu dev <ghalem.maxime@gmail.com>
2022-06-23 14:09:09 +0200

    gestion des routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
+import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'/pokemon-list' , component: ListPokemonComponent},
+  {path:'/pokemon-detail/:id' , component: DetailPokemonComponent},
+  {path:'' , redirectTo: '/pokemon-list' , pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BorderCardDirective } from './border-card.directive';
 import { PokemonColorTypePipe } from './pokemon-color-type.pipe';
+import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
+import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BorderCardDirective,
-    PokemonColorTypePipe
+    PokemonColorTypePipe,
+    ListPokemonComponent,
+    DetailPokemonComponent
   ],
   imports: [
     BrowserModule,
