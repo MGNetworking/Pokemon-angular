76b270d2feaf1d1f32998c0fdaf332dabd42c11e
Max ubuntu dev <ghalem.maxime@gmail.com>
2022-06-23 11:50:33 +0200

    creation de pipe personnaisé

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BorderCardDirective } from './border-card.directive';
+import { PokemonColorTypePipe } from './pokemon-color-type.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BorderCardDirective
+    BorderCardDirective,
+    PokemonColorTypePipe
   ],
   imports: [
     BrowserModule,
